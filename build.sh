8db1f0bba38f19e2c54f186109b2bd6e9f179f6a
Lars Stadel Linnet <larslinnet@gmail.com>
2012-05-22 16:37:00 +0200

    Removed running make for nodestream profile

--- build.sh
+++ build.sh
@@ -10,15 +10,6 @@ run_cmd "ln -s ../../${PROFILENAME}_profile ${PROFILENAME}" "${NEWWEB}/profiles"
 
 run_hooked_cmd "profile_make" "drush -y make --working-copy profiles/${PROFILENAME}/${PROFILENAME}.make" "${NEWWEB}";
 
-if ${KEEPNS}; then
-  run_cmd "cp -r ${OLDWEB}/profiles/${NS} ${NEWWEB}/profiles/${NS}";
-else
-  # @TODO Should we always use git here?
-  run_cmd "git clone http://git.drupal.org/project/nodestream nodestream" "${NEWWEB}/profiles";
-  run_cmd "git checkout ${NSVERSION}" "${NEWWEB}/${NSPROFILE}";
-  run_hooked_cmd "nodestream_make" "drush -y make --no-core --contrib-destination=. drupal-org.make" "${NEWWEB}/profiles/nodestream";
-fi
-
 if [ ${OLDWEB} ]; then
   ask "Do you want to move the web/ symlink from ${OLDWEB} to ${NEWWEB}" "relink";
   ask "Do you want to remove the directory ${OLDWEB} and all its contents" "rm -rf ${OLDWEB}";
