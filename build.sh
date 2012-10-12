ef80818824ede79cbda59ee11c4134218477db22
Claes Gyllensvärd <letharion@gmail.com>
2012-10-12 11:12:40 +0200

    Always update the platform alias when building, and also create the site-alias

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 source build/base_functions.sh
 
-if [ ! -f "${HOME}/.drush/${PLATFORM_ALIAS}.alias.drushrc.php" ]; then
-  run_cmd "drush --debug provision-save @${PLATFORM_ALIAS} --context_type=platform --root=${PLATFORM_ROOT} --makefile=${MAKEFILE}"
-fi
+# Update the platform path to the new one.
+# @TODO There should probably be a way to keep old platforms around as well.
+run_cmd "drush --debug provision-save @${PLATFORM_ALIAS} --context_type=platform --root=${PLATFORM_ROOT} --makefile=${MAKEFILE}"
 
 run_cmd "drush provision-verify @${PLATFORM_ALIAS}"
 run_cmd "drush make ${MAKEFILE} ${PLATFORM_ROOT}"
+
+run_cmd "drush provision-save ${DOMAIN} --uri=${FULLDOMAIN} --platform=@${PLATFORM_ALIAS} --context_type=site --client_name=admin --db_server=@server_master --profile=${DOMAIN}"
 
 run_cmd "ln -s ../../${FULLDOMAIN} ." "${NEWWEB}/sites";
 
